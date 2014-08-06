bd2faf7b5d6c89ea84d9ce866e64dd05ffae2725
Jonathan Cohen <jonathandcohen222@gmail.com>
2014-08-06 11:28:17 -0400

    initial background script
    the background script will hold settings for each team’s visuals.  This
    just has the test for default, rektiles, and cpm

---
+++ js/background.js
@@ -0,0 +1,14 @@
+var teams = {
+	"Rektiles": {
+		"logo": true,
+		"uniform": true,
+		"red": "rgb(52, 95, 41)"
+	},
+	"Capquistadors": {
+		"logo": true,
+		"uniform": true,
+		"blue": "rgb(219, 168, 39)"
+	},
+	"none": {
+	}
+}
