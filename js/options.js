af0c4c4ae4364055ec91b5592e306c20b52b2a75
Jonathan Cohen <jonathandcohen222@gmail.com>
2014-08-06 00:40:06 -0400

    initial browser action
    added browser action to be used for changing settings such as teams,
    match settings, etc.  The button still doesn’t work, but I feel
    comfortable enough pushing for such a small change.

---
+++ js/options.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById("saveOptions").onclick = function() {
+		var p = document.getElementById('abc');
+		p.innerText = 'abc';
+	};
+});
